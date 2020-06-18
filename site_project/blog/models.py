727f84056783b01900870e0e718356bde6cd9137
Guillaume <roehm.guillaume@gmail.com>
2020-06-18 12:07:00 -1000

    Initial commit

---
+++ site_project/blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, verbose_name="Auteur")
+    title  = models.CharField(max_length = 200, verbose_name="Titre")
+    slug = models.SlugField()
+    content = models.TextField(verbose_name="Contenu")
+    created_date = models.DateTimeField(default = timezone.now, verbose_name="Date de création")
+    published_date = models.DateTimeField(blank=  True, null = True, verbose_name="Date de publication")
+
+    def publish(self):
+
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+
+        return self.title
+
+
