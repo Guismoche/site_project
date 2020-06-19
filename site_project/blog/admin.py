f794f7299ef52001b4790a268482db37947f6059
Guillaume <roehm.guillaume@gmail.com>
2020-06-19 09:12:34 -1000

    Add categories

--- site_project/blog/admin.py
+++ site_project/blog/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Post, Category
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ("title","author","category","created_date","published_date", "is_publish")
+    list_filter = ("author","is_publish",)
+    date_hierarchy = "created_date"
+    ordering = ("created_date",)
+    search_fields = ("title","content")
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("pk","name" )
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category,CategoryAdmin)
 
--- site_project/blog/models.py
+++ site_project/blog/models.py
@@ -5,7 +5,11 @@ from django.utils import timezone
 # Create your models here.
 class Category(models.Model):
 
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, verbose_name= "Nom")
+
+    class Meta:
+        verbose_name= "Catégorie"
+        verbose_name_plural = "Catégories"
 
     def __str__(self):
         return self.name
@@ -18,15 +22,22 @@ class Post(models.Model):
     content = models.TextField(verbose_name="Contenu")
     created_date = models.DateTimeField(default = timezone.now, verbose_name="Date de création")
     published_date = models.DateTimeField(blank=  True, null = True, verbose_name="Date de publication")
+    is_publish= models.BooleanField(blank= True, default = False)
 
-    category = models.ForeignKey(Category, default = 1, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, default = 1, on_delete=models.CASCADE, verbose_name="Catégorie")
 
+    class Meta:
+        ordering = ["-created_date"]
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
 
 
     def publish(self):
 
         self.published_date = timezone.now()
+        self.is_publish = True
         self.save()
+
 
     def __str__(self):
 
